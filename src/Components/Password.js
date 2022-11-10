3f2f5d8e5b466a904283da0e1bea987d57a2ca36
ARUN KUMAR YADAV <77447025+Arun12116@users.noreply.github.com>
2022-11-10 13:55:06 +0530

    Update Password.js

--- src/Components/Password.js
+++ src/Components/Password.js
@@ -1,6 +1,6 @@
 export const lowercase = "abcdefghijklmnopqrstuvwxyz";
 export const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 export const number = "0123456789";
-export const symbols =" !\"\#\$\%&\'\()\*\+\,\-\.\/\:\;\<=>\?\@\[\]\^\_\`{|}\~\" "
+export const symbols =" @#%^*()(_+ :'/ "
 
 
